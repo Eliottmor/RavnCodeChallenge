19f70e9490cd43e4b2a158324fcee70caa559202
Eliott Moreno <eliott_mor@hotmail.com>
2018-12-07 22:26:41 -0700

    Got apollo working

---
+++ Pods/Target Support Files/Pods-RavnApp/Pods-RavnApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RavnApp : NSObject
+@end
+@implementation PodsDummy_Pods_RavnApp
+@end
---
+++ RavnApp/GitHubService.swift
@@ -0,0 +1,16 @@
+//
+//  GitHubService.swift
+//  RavnApp
+//
+//  Created by Eliott Moreno on 12/5/18.
+//  Copyright © 2018 Eliott Moreno. All rights reserved.
+//
+
+import Foundation
+
+class GitHubService {
+    //endPoint: https://api.github.com/graphql
+    //token: 559c2f3900adc21f55e3972f39ff0d6530f97e11
+    
+    
+}
